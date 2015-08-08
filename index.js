8d8749bcda8ac55dfe010ad963a47d4c382506b2
Ben Drucker <bvdrucker@gmail.com>
2015-08-07 19:43:11 -0700

    Use Base64 instead of Buffer for base 64 encoding
    Shaves roughly 5k from the library (largest dep)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 var request = require('micro-req')
+var base64Encode = require('Base64').btoa
 var lobKey = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'// test api key
 
 module.exports = function verify (address, callback) {
   request.post('https://api.lob.com/v1/verify', {
     json: true,
     headers: {
-      Authorization: 'Basic ' +
-        new Buffer(lobKey + ':').toString('base64')
+      Authorization: 'Basic ' + base64Encode(lobKey + ':')
     },
     body: address
   }, function (err, response) {
